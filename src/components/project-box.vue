ee130c0e0bb46a4251ebe261d92bbc8c085df94c
andre nguyen <andre97@uw.edu>
2018-12-13 21:54:23 -0800

    finished fat chunk of modal

--- src/components/project-box.vue
+++ src/components/project-box.vue
@@ -1,29 +1,88 @@
 <template>
-  <div class="project-box">
-    <div class="project-box__content">
-      <div class="project-box__icon">
-        <img :src="require(`@/assets/projects/${project.icon}`)" alt>
+  <div class="project">
+    <div class="project__box" @click="isActive = !isActive">
+      <div class="project__content">
+        <div class="project__icon">
+          <img :src="require(`@/assets/projects/${project.icon}`)" alt>
+        </div>
+      </div>
+
+      <h3 class="project__title">{{ project.name }}</h3>
+      <div class="project__links">
+        <a href class="project__links-box link-heading">
+          <img :src="require(`@/assets/projects/github.svg`)" alt>github
+        </a>
+
+        <a href class="project__links-box link-heading">
+          <img :src="require(`@/assets/projects/eye.svg`)" alt>live demo
+        </a>
       </div>
     </div>
 
-    <h3 class="project-box__title">{{ project.name }}</h3>
-    <div class="project-box__links">
-      <a href class="project-box__links-box link-heading">
-        <img :src="require(`@/assets/projects/github.svg`)" alt>github
-      </a>
-
-      <a href class="project-box__links-box link-heading">
-        <img :src="require(`@/assets/projects/eye.svg`)" alt>live demo
-      </a>
-    </div>
-    <div class="modal-box">
+    <div class="modal-box" :class="{ active: isActive }">
       <div class="modal-box__content">
-        <div class="modal-box__close">&times;</div>
-        <div class="modal-box__title"></div>
-        <div class="modal-box__description"></div>
-        <div class="modal-box__images"></div>
-        <div class="modal-box__tags"></div>
-        <div class="modal-box__links"></div>
+        <div class="modal-box__close" @click="isActive = !isActive">&times;</div>
+        <div class="modal-box__title">
+          <h3>{{ project.name }}</h3>
+        </div>
+        <div class="modal-box__description">
+          <p>{{ project.description }}</p>
+        </div>
+        <div class="modal-box__images">
+          <div class="modal-box__images-desktop">
+            <img
+              class="modal-box__images-desktop-outline"
+              src="@/assets/projects/desktop-svg-bg.svg"
+              alt
+            >
+            <img
+              class="modal-box__images-desktop-img"
+              :src="require(`@/assets/projects/${project.desktopImage}`)"
+              alt
+            >
+          </div>
+          <div class="modal-box__images-mobile">
+            <div class="modal-box__images-mobile-box">
+              <img
+                class="modal-box__images-mobile-box-outline"
+                src="@/assets/projects/mobile-svg-bg.svg"
+                alt
+              >
+              <img
+                class="modal-box__images-mobile-box-img"
+                :src="require(`@/assets/projects/${project.mobileImage1}`)"
+                alt
+              >
+            </div>
+            <div class="modal-box__images-mobile-box">
+              <img
+                class="modal-box__images-mobile-box-outline"
+                src="@/assets/projects/mobile-svg-bg.svg"
+                alt
+              >
+              <img
+                class="modal-box__images-mobile-box-img"
+                :src="require(`@/assets/projects/${project.mobileImage2}`)"
+                alt
+              >
+            </div>
+          </div>
+        </div>
+        <div class="modal-box__tags">
+          <h3>Built with</h3>
+          <div class="modal-box__tags-box">
+            <div v-for="tag in project.tags" :key="tag" class="modal-box__tags-box-item">{{ tag }}</div>
+          </div>
+        </div>
+        <div class="modal-box__links">
+          <a href class="modal-box__links-box link-heading">
+            <img :src="require(`@/assets/projects/github.svg`)" alt>github
+          </a>
+
+          <a href class="modal-box__links-box link-heading">
+            <img :src="require(`@/assets/projects/eye.svg`)" alt>live demo
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -33,6 +92,11 @@
 export default {
   props: {
     project: Object
+  },
+  data: function () {
+    return {
+      isActive: false
+    }
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,14 +7,14 @@
 </template>
 
 <script>
-import Header from "@/sections/header.vue";
-import About from "@/sections/about.vue";
-import Projects from "@/sections/projects.vue";
+import Header from '@/sections/header.vue';
+import About from '@/sections/about.vue';
+import Projects from '@/sections/projects.vue';
 export default {
   components: {
     Header,
     About,
     Projects
   }
-};
+}
 </script>
