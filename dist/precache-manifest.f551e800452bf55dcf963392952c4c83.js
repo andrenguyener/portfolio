d0c2c12ee662b1a5064de4e8dbf45ac8a7bc6365
andre nguyen <andre97@uw.edu>
2018-12-24 12:15:16 -0800

    compressed image

---
+++ dist/precache-manifest.f551e800452bf55dcf963392952c4c83.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fa995e5a86e88225192df020b3b0846",
+    "url": "/img/logo.7fa995e5.png"
+  },
+  {
+    "revision": "209bb4ded031618862ab26fd580a09f3",
+    "url": "/resume.pdf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f3c34eea3d31fc8b01044e5f003421da",
+    "url": "/img/mobile1.f3c34eea.jpg"
+  },
+  {
+    "revision": "32a6220e9625b42f8074b26701893ee5",
+    "url": "/img/mobile1.32a6220e.jpg"
+  },
+  {
+    "revision": "cbc7d47dad57fa073aa6",
+    "url": "/js/chunk-vendors.273b2582.js"
+  },
+  {
+    "revision": "7d7e45167680305539e107f5d8c7274d",
+    "url": "/img/mobile2.7d7e4516.jpg"
+  },
+  {
+    "revision": "532b31626236f23d853b2fc3830d3f95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af40d200b3af0ce1f4827acadceb8b4b",
+    "url": "/img/mobile1.af40d200.jpg"
+  },
+  {
+    "revision": "fb9f73ddf495c322538e861d543a74fa",
+    "url": "/img/mobile2.fb9f73dd.jpg"
+  },
+  {
+    "revision": "cd2c377711e8bef5eb416c0fb0eb36af",
+    "url": "/img/mobile2.cd2c3777.jpg"
+  },
+  {
+    "revision": "660cddf5cd9e1a44937a",
+    "url": "/js/app.48895f32.js"
+  },
+  {
+    "revision": "638dbaf5a4a965b0c6171582b772fa97",
+    "url": "/img/mobile2.638dbaf5.jpg"
+  },
+  {
+    "revision": "b6adba9308dbb32760195d1570420aad",
+    "url": "/img/mobile1.b6adba93.jpg"
+  },
+  {
+    "revision": "86c9f5018d3fd0178cd395d6ed47aafb",
+    "url": "/img/mobile2.86c9f501.jpg"
+  },
+  {
+    "revision": "86c99df1493e569703d3ddc771bbbe24",
+    "url": "/img/forest-s.86c99df1.jpg"
+  },
+  {
+    "revision": "12667ee9420dbf3ae61cac674741304e",
+    "url": "/img/mobile1.12667ee9.jpg"
+  },
+  {
+    "revision": "46094402cc4f44013e64692f7b8b8c7f",
+    "url": "/fonts/TradeGothicLTStd.46094402.otf"
+  },
+  {
+    "revision": "02b3f3c850a9b336df307140d56377dc",
+    "url": "/img/headshot.02b3f3c8.jpg"
+  },
+  {
+    "revision": "008c318e4204701a92dacffae715e0ce",
+    "url": "/img/logo.008c318e.png"
+  },
+  {
+    "revision": "042458a15d6decc59436c19df60ad0ca",
+    "url": "/img/logo.042458a1.png"
+  },
+  {
+    "revision": "965e4efe7b758331caff0eab3089971b",
+    "url": "/img/logo.965e4efe.png"
+  },
+  {
+    "revision": "7bea79083234140f7af14d534f63ca04",
+    "url": "/img/forest-xs.7bea7908.jpg"
+  },
+  {
+    "revision": "9fccd19708cd823fe80270d0f5a25fb1",
+    "url": "/img/desktop.9fccd197.jpg"
+  },
+  {
+    "revision": "6d0d3dd28591f64a24fde27d0a2faf7c",
+    "url": "/img/desktop.6d0d3dd2.jpg"
+  },
+  {
+    "revision": "62db82c5b273a7f865e02102da462b13",
+    "url": "/img/desktop.62db82c5.jpg"
+  },
+  {
+    "revision": "15e1db91cd5a633e9ba0a731936d1ce6",
+    "url": "/img/desktop.15e1db91.jpg"
+  },
+  {
+    "revision": "03641a4659ff7619835d43e1a7a1fac5",
+    "url": "/img/desktop.03641a46.jpg"
+  },
+  {
+    "revision": "e9dde3cd578948f7a68232a7f3f0e4a0",
+    "url": "/fonts/TradeGothic-Bold.e9dde3cd.otf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/fonts/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/fonts/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "660cddf5cd9e1a44937a",
+    "url": "/css/app.5b9a525a.css"
+  }
+];
