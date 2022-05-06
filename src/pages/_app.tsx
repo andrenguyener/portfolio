58f37600647079243c8d0e1ac1d2bbd21245181f
andrenguyener <andrenguyenp@gmail.com>
2022-05-06 09:23:16 -0700

    chore: remove unused comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from "next/app";
-// import { Head } from "next/document";
 import { globalFonts } from "./../static/fonts";
 import { Layout } from "./../views/layout";
 import "../static/globals.css";
@@ -7,10 +6,6 @@ import "../static/globals.css";
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
-            {/* <Head>
-                <title>Andre Nguyen</title>
-                <meta property="og:title" content="Andre Nguyen" key="portfolio" />
-            </Head> */}
             {globalFonts}
             <Layout>
                 <Component {...pageProps} />
